704e605460be824201457d9575b4cdb478ab25ca
Kevin Refermat <kevinrefermat@gmail.com>
2014-01-28 10:43:58 -0800

    rm deprecated header source folders

--- Headers/NavigationSystem.h
+++
@@ -1,3 +0,0 @@
-void navigate( signed char * rotations, unsigned char * distances, unsigned char size );
-
-void determinePosition( int * currentXPosition, int * currentYPosition );
--- Headers/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
-typedef unsigned short gridUnit_t;
-typedef unsigned short degree_t;
-typedef unsigned short pulseCount_t;
-
-#endif
