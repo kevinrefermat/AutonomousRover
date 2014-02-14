42259a1459675b565a1996b197afac098b3561cd
Kevin Refermat <kevinrefermat@gmail.com>
2014-02-13 19:19:24 -0800

    added command line inputs to graph main.c program

--- navigationPractice/navigationC/main.c
+++ navigationPractice/navigationC/main.c
@@ -1,21 +1,43 @@
 #include "Graph.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+int main( int argc, char** argv )
 {
+   if ( argc != 3 )
+   {
+      printf( "Syntax: <executable> <source node ID number> <target node ID number>\n" );
+      exit( 1 );
+   }
+
+   graphSize_t source, target;
+   source = atoi( argv[ 1 ] );
+   target = atoi( argv[ 2 ] );
+    
    graph_t* graph = CreateGraph( 12 );
 
-   AddEdge( graph, 0, 1, 14);
-   AddEdge( graph, 0, 2, 9);
-   AddEdge( graph, 0, 3, 7);
-   AddEdge( graph, 1, 2, 2);
-   AddEdge( graph, 1, 4, 9);
-   AddEdge( graph, 2, 3, 10);
-   AddEdge( graph, 2, 5, 11);
-   AddEdge( graph, 3, 5, 15);
-   AddEdge( graph, 4, 5, 6);
+   AddEdge( graph, 0, 1, 23);
+   AddEdge( graph, 0, 3, 5);
+   AddEdge( graph, 0, 2, 8);
+   AddEdge( graph, 1, 4, 17);
+   AddEdge( graph, 1, 5, 16);
+   AddEdge( graph, 2, 3, 19);
+   AddEdge( graph, 2, 6, 15);
+   AddEdge( graph, 3, 7, 11);
+   AddEdge( graph, 4, 5, 15);
+   AddEdge( graph, 4, 8, 20);
+   AddEdge( graph, 4, 7, 24);
+   AddEdge( graph, 5, 8, 14);
+   AddEdge( graph, 5, 11, 21);
+   AddEdge( graph, 6, 7, 25);
+   AddEdge( graph, 6, 9, 15);
+   AddEdge( graph, 7, 9, 9);
+   AddEdge( graph, 7, 10, 23);
+   AddEdge( graph, 8, 10, 22);
+   AddEdge( graph, 9, 10, 6);
+   AddEdge( graph, 10, 11, 5);
    
-   Dijkstra( graph, 0, 4 ); 
+   Dijkstra( graph, source, target ); 
    
    DestroyGraph( graph ); 
    return 0;
