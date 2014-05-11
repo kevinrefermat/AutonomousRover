e21cbe42ed762c80c805b972f9a6a8a69d3b01d6
Kevin Refermat <kevinrefermat@gmail.com>
2014-05-11 09:42:40 -0700

    clean up

---
+++ NavigationSystemPractice/MiscellaneousNavigationPracticeFiles/ConvertLineOfSightToFloorDistance/ConvertLineOfSightToFloorDistance.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdint.h>
+
+typedef inches_t int16_t;
+static const inches_t CeilingHeight = 96;
+
+inches_t GetFloorDistance( inches_t hypotenuseDistance )
+{
+   inches_t floorDistance;
+   floorDistance = SquareRoot
+}
+
+int main()
+{
+   
